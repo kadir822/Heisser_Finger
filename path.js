7a202cd8191793aff6f231e1c16574f10dd42b62
kadir822 <32572079+kadir822@users.noreply.github.com>
2019-11-17 17:03:00 +0100

    Path.js add

---
+++ path.js
@@ -0,0 +1,25 @@
+'use strict';
+(function () {
+	function draw() {
+		var element = document.getElementById('canvas');
+		if (element.getContext) {
+			var context = element.getContext('2d');
+			context.clearRect(0, 0, element.width, element.height);
+			context.beginPath();
+            context.moveTo(80, 400);
+           //context.quadraticCurveTo(80,350,400,150);
+			context.lineTo(350, 150);
+            context.lineTo(500, 400);
+            context.lineTo(700, 200);
+			//context.arc(90, 50, 30, Math.PI * 7 / 6, Math.PI * 5 / 6);
+			//context.closePath();
+			context.strokeStyle = '#000000';
+			context.lineWidth = 10;
+			context.lineJoin = 'round';
+			context.stroke();
+		}
+	}
+	document.addEventListener("DOMContentLoaded", function () {
+		draw();
+	});
+}());
