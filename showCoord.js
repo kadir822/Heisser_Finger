9e98fc7cf7ed6ac3b989fdd865fef981e0ef825c
kadir822 <32572079+kadir822@users.noreply.github.com>
2019-11-25 21:56:40 +0100

    showCoord.js; add coord function

---
+++ showCoord.js
@@ -0,0 +1,12 @@
+function showCoords(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var coor = "X coords: " + x + ", Y coords: " + y;
+    document.getElementById("demo").innerHTML = coor;
+}
+
+function clearCoor() {
+    document.getElementById("demo").innerHTML = "";
+}
+
+
