b7afd22b25f6c4cd9ae349d428c7f9a493c8a536
kadir822 <32572079+kadir822@users.noreply.github.com>
2019-11-25 21:57:28 +0100

    change the path from curved to staright lines

--- path.js
+++ path.js
@@ -1,26 +1,29 @@
 'use strict';
-(function () {
-	function draw() {
-		var element = document.getElementById('canvas');
-		if (element.getContext) {
-			var context = element.getContext('2d');
-			context.clearRect(0, 0, element.width, element.height);
-			context.beginPath();
-            context.moveTo(80, 400);
-           //context.quadraticCurveTo(80,350,400,150);
-			context.lineTo(350, 150);
-            context.lineTo(500, 400);
-            context.lineTo(700, 200);
-            context.lineTo(800, 180);
-			//context.arc(90, 50, 30, Math.PI * 7 / 6, Math.PI * 5 / 6);
-			//context.closePath();
-			context.strokeStyle = '#000000';
-			context.lineWidth = 50;
-			context.lineJoin = 'round';
-			context.stroke();
+(
+	function () {
+		function draw() {
+			var path = document.getElementById('canvas');
+			if (path.getContext) {
+				var context = path.getContext('2d');
+				context.clearRect(0, 0, path.width, path.height);
+				context.beginPath();
+				context.moveTo(120, 475);		//context.moveTo(80, 400);
+				context.lineTo(120, 150);		//context.lineTo(350, 150);
+				context.lineTo(500, 150);		//context.lineTo(500, 400);
+				context.lineTo(500, 450);		//context.lineTo(700, 200);
+				context.lineTo(800, 450);		//context.lineTo(800, 180);
+				context.lineTo(800, 125);	
+				context.strokeStyle = '#000000';
+				context.lineWidth = 50;
+				context.lineJoin = 'round';
+				context.stroke();
+			}
 		}
+		document.addEventListener("DOMContentLoaded", function () {
+			draw();
+		});
 	}
-	document.addEventListener("DOMContentLoaded", function () {
-		draw();
-	});
-}());
+	(
+		
+	)
+);
